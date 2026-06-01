f783314b9c8a323965ead44760ad4530bdbfaf65
ajithreddy1441 <syamalaajith11@gmail.com>
2026-06-01 20:57:28 +0530

    recreate with vite

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Navbar } from "./components/portfolio/Navbar";
+import { Hero } from "./components/portfolio/Hero";
+import { About } from "./components/portfolio/About";
+import { Skills } from "./components/portfolio/Skills";
+import { Projects } from "./components/portfolio/Projects";
+import { Experience } from "./components/portfolio/Experience";
+import { Contact } from "./components/portfolio/Contact";
+import { Footer } from "./components/portfolio/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
